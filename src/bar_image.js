31194145e03c40ba3433924ae3a2b9fd8877e311
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-04-20 13:26:24 -0300

    Mouse event

--- src/bar_image.js
+++ src/bar_image.js
@@ -343,16 +343,7 @@ const visObject = {
 
         console.log(vis)
       });
-
-
-    // window.onload = () => {
-    //   // set animation
-    //   svg.transition()
-    //     .ease(d3.easeLinear)
-    //     .duration(1000)
-    //     .attr("y", d => svg_height - bottom_offset - scale(d))
-    //     .attr("height", d => scale(d));
-    // };
+ 
 
     doneRendering();
   },
