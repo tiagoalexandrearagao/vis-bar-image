c3793cf1bf75fc3d4425bf8509e41d3e931302a2
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-04-19 21:34:00 -0300

    Mouse event

--- src/bar_image.js
+++ src/bar_image.js
@@ -314,9 +314,9 @@ const visObject = {
     //var svg_width = svg.select("#resize").node().getBoundingClientRect().width;
   
 
-    const boxes = document.getElementsByClassName('main')[0];
+    const boxes = document.getElementsByClassName('main');
 
-    var svg_width = boxes.clientWidth - 200;
+    var svg_width = boxes[0].width - 50;
 
     d3.select("body")
       .style("display", "block")
