cd6142ae92743f8140314f08b3019a04788c8850
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-04-20 14:49:58 -0300

    Mouse event

--- src/bar_image.js
+++ src/bar_image.js
@@ -3,7 +3,7 @@ import "./common/styles.css";
 import { select, selectAll } from "d3-selection";
 export { select, selectAll };
 import * as d3 from "d3";
-import * as d3_tip from "d3-tip";
+
 import $ from "jquery";
 
 const visObject = {
@@ -146,7 +146,7 @@ const visObject = {
     const top_offset = 50;
     const bottom_offset = 50;
 
-    var tip = d3_tip.tip()     
+    var tip = d3.tip()     
       .attr('class', 'd3-tip')
       .html(function (d) {
         return d.data.my_dimension + " : <span style='color:orangered'>" + d.data.count + "</span>";
