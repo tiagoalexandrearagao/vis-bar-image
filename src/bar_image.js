d29be97925aaff071b4dfee6a5b3f05e7aad81f2
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-04-24 17:41:12 -0300

    Build

--- src/bar_image.js
+++ src/bar_image.js
@@ -62,7 +62,7 @@ const visObject = {
       console.log('changedFilters',changedFilters)
       if (changedFilters) {
         const myFilter = changedFilters[0]; // acessando o primeiro filtro alterado
-        console.log('myFilter',myFilter.field); // exibe o nome do campo do filtro
+        //console.log('myFilter',myFilter.field); // exibe o nome do campo do filtro
         console.log('myFilter',myFilter.values); // exibe o valor do filtro
       }
     };
