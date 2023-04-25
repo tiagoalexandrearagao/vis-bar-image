b9fce4e60580092ee318d5f5f3404ac909698ca1
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-04-25 09:17:48 -0300

    Build

--- src/bar_image.js
+++ src/bar_image.js
@@ -5,105 +5,73 @@ export { select, selectAll };
 import * as d3 from "d3";
 
 import $ from "jquery";
-
 looker.plugins.visualizations.add({
-  id: 'my_bar_chart',
-  label: 'My Bar Chart',
-  options: {
-    font_size: {
-      type: 'number',
-      label: 'Font Size',
-      default: 12,
-      section: 'Style',
-      placeholder: 'Enter font size (pixels)'
-    },
-    bar_color: {
-      type: 'string',
-      label: 'Bar Color',
-      default: '#0077c8',
-      section: 'Style',
-      placeholder: 'Enter bar color in hex'
-    }
-  },
   create: function (element, config) {
-    // Create a new SVG element
-    var svg = d3.select(element)
-      .append('svg')
-      .attr('width', '100%')
-      .attr('height', '100%');
+    element.innerHTML = `
+      <style>
+        .my-chart {
+          height: 400px;
+        }
+      </style>
+      <div class="my-chart"></div>
+    `;
+  },
 
-    // Add axes and labels
-    var xScale = d3.scaleBand()
-      .range([0, element.clientWidth])
-      .padding(0.1);
+  update: function (data, element, config, queryResponse, details, done) {
+    const x = d3.scaleBand().range([0, element.clientWidth - 50]).padding(0.1);
+    const y = d3.scaleLinear().range([element.clientHeight - 50, 0]);
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    var yScale = d3.scaleLinear()
-      .range([element.clientHeight, 0]);
+    const svg = d3.select(element).select('.my-chart');
+    const bars = svg.selectAll('.bar').data(data);
 
-    var xAxis = svg.append('g')
-      .attr('transform', 'translate(0,' + element.clientHeight + ')');
+    // Remove as barras que não possuem mais dados
+    bars.exit().remove();
 
-    var yAxis = svg.append('g');
+    // Cria novas barras para os novos dados
+    const newBars = bars.enter().append('rect')
+      .attr('class', 'bar')
+      .attr('fill', d => color(d.dimension))
+      .attr('x', d => x(d.dimension))
+      .attr('width', x.bandwidth())
+      .attr('y', y(0))
+      .attr('height', 0);
 
-    svg.append('text')
-      .attr('transform', 'translate(' + (element.clientWidth / 2) + ',' + (element.clientHeight + 30) + ')')
-      .style('text-anchor', 'middle')
-      .text('X Axis Label');
+    // Atualiza a posição e altura das barras existentes ou novas
+    newBars.merge(bars)
+      .transition().duration(300)
+      .attr('x', d => x(d.dimension))
+      .attr('width', x.bandwidth())
+      .attr('y', d => y(d.measure))
+      .attr('height', d => y(0) - y(d.measure));
 
-    svg.append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 0 - 40)
-      .attr('x', 0 - (element.clientHeight / 2))
-      .attr('dy', '1em')
-      .style('text-anchor', 'middle')
-      .text('Y Axis Label');
+    // Atualiza os eixos
+    x.domain(data.map(d => d.dimension));
+    y.domain([0, d3.max(data, d => d.measure)]);
 
-    // Return the chart instance
-    return {
-      updateAsync: function (data, element, config, queryResponse, details, doneRendering) {
-        // Filter the data based on the selected filters
-        var selectedValues = [];
-        var filters = queryResponse.filters;
-        var dimensions = queryResponse.fields.dimension_like;
-        var measures = queryResponse.fields.measure_like;
-        var measuresQueryNames = [];
+    svg.select('.x-axis')
+      .transition().duration(300)
+      .call(d3.axisBottom(x));
 
-        measures.forEach(function (measure) {
-          measuresQueryNames.push(measure.name);
-        });
+    svg.select('.y-axis')
+      .transition().duration(300)
+      .call(d3.axisLeft(y));
 
-        dimensions.forEach(function (dimension) {
-          var filter = filters[dimension.name];
-          if (filter && filter.field && filter.field.is_timeframe) {
-            var value = filter.field.timeframe;
-            selectedValues.push(value);
-          } else if (filter && filter.value) {
-            var values = filter.value;
-            values.forEach(function (value) {
-              selectedValues.push(value);
-            });
-          }
-        });
-
-        var filteredData = data.filter(function (d) {
-          var keepRow = true;
-          dimensions.forEach(function (dimension) {
-            var value = d[dimension.name].value;
-            if (selectedValues.indexOf(value) === -1) {
-              keepRow = false;
-            }
-          });
-          return keepRow;
-        });
-
-        // Group the data by dimension
-        var dimensionName = dimensions[0].name;
-        var measureName = measuresQueryNames[0];
-        var groupedData = d3.nest()
-          .key(function (d) { return d[dimensionName].value; })
-          .rollup(function (v) { return d3.sum(v, function (d) { return d[measureName].value; }); })
-          .entries
-      }
+    // Adiciona os eixos se ainda não existirem
+    if (svg.selectAll('.x-axis').empty()) {
+      svg.append('g')
+        .attr('class', 'x-axis')
+        .attr('transform', `translate(0, ${element.clientHeight - 50})`)
+        .call(d3.axisBottom(x));
     }
+
+    if (svg.selectAll('.y-axis').empty()) {
+      svg.append('g')
+        .attr('class', 'y-axis')
+        .attr('transform', 'translate(50, 0)')
+        .call(d3.axisLeft(y));
+    }
+
+    done();
   }
-})
+});
