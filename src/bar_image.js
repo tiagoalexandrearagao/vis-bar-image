230afbd54ba962c44a65e11c198d5d92c18ea2e4
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-04-24 21:35:36 -0300

    Build

--- src/bar_image.js
+++ src/bar_image.js
@@ -28,7 +28,7 @@ function updateChart(data, queryResponse) {
   const bar_color = "#FFCB65";
   var default_title = ''//`<img style="width:150px; height:auto;" src="${config.default_icon}">${config.title_graphic}`;
 
-   var teste = d3.select('#vis').remove();
+   var teste = d3.select('#vis').exit().remove();
  
 
   const tooltip = d3.select("body")
