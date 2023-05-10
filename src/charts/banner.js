2d6717f14be6199629e7b4a7d5fbe49cc86248fc
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-05-10 17:05:45 -0300

    Novo Chart

--- src/charts/banner.js
+++ src/charts/banner.js
@@ -104,6 +104,11 @@ export function banner(params) {
     `"background:linear-gradient(210.38deg, #2FE1EC -23.6%, rgba(47, 187, 236, 0) 62.05%), linear-gradient(149.62deg, #2573E9 -34.04%, #C006DE 147.39%);`
   );
 
+  d3.select("body").attr(
+    "style",
+    `"background:linear-gradient(210.38deg, #2FE1EC -23.6%, rgba(47, 187, 236, 0) 62.05%), linear-gradient(149.62deg, #2573E9 -34.04%, #C006DE 147.39%);`
+  );
+
   // //texto lateral percentual
   // svgTitle
   //   .append("span")
