a386365b561c61f4cd47adfc9b76ef398e386217
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-05-10 19:11:16 -0300

    Novo Chart

--- src/charts/banner.js
+++ src/charts/banner.js
@@ -115,36 +115,14 @@ export function banner(params) {
     background: linear-gradient(210.38deg, #2FE1EC -23.6%, rgba(47, 187, 236, 0) 62.05%), linear-gradient(149.62deg, #2573E9 -34.04%, #C006DE 147.39%);`
   );
 
-  // //texto lateral percentual
-  // svgTitle
-  //   .append("span")
-  //   .data(pie(formattedData))
-  //   .attr("fill", "#333")
-  //   .text(function (d) {
-  //     return (
-  //       String(
-  //         parseFloat(
-  //           ((d.endAngle - d.startAngle) / (2 * Math.PI)) * 100
-  //         ).toFixed(0)
-  //       ) + "%"
-  //     );
-  //   })
-  //   .attr(
-  //     "style",
-  //     `margin-left:13px; margin-top:80px;position:absolute; font-family: ${fontFamily};font-weight:${fontWeightBold} ; font-size:18px; color:#333`
-  //   );
-
-  // //texto lateral value
-  // svgTitle
-  //   .append("span")
-  //   .data(pie(formattedData))
-  //   .text(function (d) {
-  //     return d.data.dimension_values;
-  //   })
-  //   .attr(
-  //     "style",
-  //     `margin-left:13px; margin-top:100px;position:absolute; font-family: ${fontFamily};font-weight:${fontWeightNormal} ;font-size:12px`
-  //   );
+  svgTitle
+    .append("div")
+    .attr("style", "position:absolute; float:right; ")
+    .html(
+      `<svg width="562" height="512" viewBox="0 0 562 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path opacity="0.2" fill-rule="evenodd" clip-rule="evenodd" d="M281.001 117.633C217.761 117.633 167.05 168.941 167.05 231.584C167.05 294.227 217.761 345.535 281.001 345.535C344.241 345.535 395.549 294.227 395.549 231.584C394.952 168.344 344.241 117.633 281.001 117.633ZM281.001 117.633C217.761 117.633 167.05 168.941 167.05 231.584C167.05 294.227 217.761 345.535 281.001 345.535C344.241 345.535 395.549 294.227 395.549 231.584C394.952 168.344 344.241 117.633 281.001 117.633ZM472.499 364.04C411.645 370.603 344.229 371.796 280.989 371.796C217.749 371.796 150.333 370.603 89.4797 364.04C72.1782 362.25 62.6325 358.074 60.2461 338.983C56.6665 303.783 56.6665 267.987 56.6665 231.594C56.6665 195.201 56.6665 158.809 60.2461 124.206C62.0359 105.114 71.5816 100.938 89.4797 99.1484C150.333 92.5858 217.749 91.3926 280.989 91.3926C344.229 91.3926 411.645 92.5858 472.499 99.1484C490.397 100.938 499.943 105.114 501.732 124.206C505.312 159.405 505.312 195.201 505.312 231.594C505.312 267.987 505.312 303.783 501.732 338.983C499.346 357.477 489.8 362.25 472.499 364.04ZM281 -50C125.883 -50 0 76.4798 0 231C0 385.52 125.883 512 281 512C436.117 512 562 385.52 562 231C562 76.4798 436.117 -50 281 -50ZM281.001 117.633C217.761 117.633 167.05 168.941 167.05 231.584C167.05 294.227 217.761 345.535 281.001 345.535C344.241 345.535 395.549 294.227 395.549 231.584C394.952 168.344 344.241 117.633 281.001 117.633Z" fill="white"/>
+    </svg>    `
+    );
 
   //novo fim
   return svgTitle;
