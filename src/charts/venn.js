cc49706bd05588f18eccc53a9777294313968d61
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-05-17 16:17:38 -0300

    Criação e manutenção d novos charts

--- src/charts/venn.js
+++ src/charts/venn.js
@@ -103,46 +103,13 @@ export function vennChart(params) {
   d3.select("#chart").append("div").attr("id", "venn");
 
   var div = d3.select("#venn");
-  div.datum(sets).call(venn.VennDiagram());
+  //div.datum(sets).call(venn.VennDiagram());
+  try {
+    console.log(venn);
+  } catch (error) {
+    console.log(error);
+  }
 
-  // add a tooltip
-  var tooltip = d3.select("body").append("div").attr("class", "venntooltip");
-
-  // add listeners to all the groups to display tooltip on mouseover
-  div
-    .selectAll("g")
-    .on("mouseover", function (d, i) {
-      // sort all the areas relative to the current item
-      venn.sortAreas(div, d);
-
-      // Display a tooltip with the current size
-      tooltip.transition().duration(400).style("opacity", 0.9);
-      tooltip.text(d.size + " users");
-
-      // highlight the current path
-      var selection = d3.select(this).transition("tooltip").duration(400);
-      selection
-        .select("path")
-        .style("stroke-width", 3)
-        .style("fill-opacity", d.sets.length == 1 ? 0.4 : 0.1)
-        .style("stroke-opacity", 1);
-    })
-
-    .on("mousemove", function () {
-      tooltip
-        .style("left", d3.event.pageX + "px")
-        .style("top", d3.event.pageY - 28 + "px");
-    })
-
-    .on("mouseout", function (d, i) {
-      tooltip.transition().duration(400).style("opacity", 0);
-      var selection = d3.select(this).transition("tooltip").duration(400);
-      selection
-        .select("path")
-        .style("stroke-width", 0)
-        .style("fill-opacity", d.sets.length == 1 ? 0.25 : 0.0)
-        .style("stroke-opacity", 0);
-    });
   //novo fim
   return div;
 }
