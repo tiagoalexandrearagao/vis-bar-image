37eeaf44177c098c26fe22dedd2ee1d3ff884db6
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-05-18 12:37:53 -0300

    Criação e manutenção d novos charts

--- src/charts/venn_chart.js
+++ src/charts/venn_chart.js
@@ -167,7 +167,9 @@ export function vennChart(params) {
       if (d.sets.length == 1 && d.sets[0] == 1) {
         //email
         return (
-          (parseInt(width) + parseInt(margin.left) + parseInt(margin.right)) / 2
+          (parseInt(width) + parseInt(margin.left) + parseInt(margin.right)) /
+            2 -
+          40
         );
       } else if (d.sets.length == 1 && d.sets[0] == 0) {
         //documeto
