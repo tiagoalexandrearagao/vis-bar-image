6ae6a68630dc386f063940cb46c493d50c3d9a07
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-05-18 16:51:37 -0300

    Criação e manutenção d novos charts

--- src/charts/venn_chart.js
+++ src/charts/venn_chart.js
@@ -222,14 +222,15 @@ export function vennChart(params) {
 
   d3.selectAll("#dataset").html(
     `
-    <span style="font-size:14px">dataset<br> ${Intl.NumberFormat(
-      "pt-BR"
-    ).format(dataset)}</span>`
+    <span style="font-size:12px">dataset</span><br>  
+    <span style="font-size:14px">${Intl.NumberFormat("pt-BR").format(
+      dataset
+    )}</span>`
   );
 
   d3.selectAll("#documento").html(
     `<strong style="color:rgb(31, 119, 180); font-size:14px;"><i class="fa-solid fa-address-card"></i> documento</strong><br> 
-    <span style="font-size:14px"> ${Intl.NumberFormat("pt-BR").format(
+    <span style="font-size:1px"> ${Intl.NumberFormat("pt-BR").format(
       total_documento
     )}</span><br>
     ${parseFloat(percent_documento).toFixed(2)}%`
