9f24c8b0ed7b7f13f9eebcd8fe7b74f82a2321bc
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-05-19 15:13:13 -0300

    Criação e manutenção d novos charts

--- src/charts/map.js
+++ src/charts/map.js
@@ -195,121 +195,118 @@ export function mapChart(params) {
   color.domain([0, 1, 2, 3, 4]); // setting the range of the input data
 
   // Load GeoJSON data and merge with states data
-  d3.json(
-    "https://tiagoalexandrearagao.github.io/viz-bar_image-marketplace/public/brasil.json",
-    function (json) {
-      // Loop through each state data value in the .csv file
-      // for (var i = 0; i < data.length; i++) {
-      //   // Grab State Name
-      //   var dataState = data[i].state;
+  d3.json("./brasil.json", function (json) {
+    // Loop through each state data value in the .csv file
+    // for (var i = 0; i < data.length; i++) {
+    //   // Grab State Name
+    //   var dataState = data[i].state;
 
-      //   // Grab data value
-      //   var dataValue = data[i].visited;
+    //   // Grab data value
+    //   var dataValue = data[i].visited;
 
-      //   // Find the corresponding state inside the GeoJSON
-      //   for (var j = 0; j < json.features.length; j++) {
-      //     var jsonState = json.features[j].properties.name;
+    //   // Find the corresponding state inside the GeoJSON
+    //   for (var j = 0; j < json.features.length; j++) {
+    //     var jsonState = json.features[j].properties.name;
 
-      //     if (dataState == jsonState) {
-      //       // Copy the data value into the JSON
-      //       json.features[j].properties.visited = dataValue;
+    //     if (dataState == jsonState) {
+    //       // Copy the data value into the JSON
+    //       json.features[j].properties.visited = dataValue;
 
-      //       // Stop looking through the JSON
-      //       break;
-      //     }
-      //   }
-      // }
+    //       // Stop looking through the JSON
+    //       break;
+    //     }
+    //   }
+    // }
 
-      console.log("Obtendo o mapa", json.geometries);
+    console.log("Obtendo o mapa", json.geometries);
 
-      // Bind the data to the SVG and create one path per GeoJSON feature
-      svg
-        .selectAll("path")
-        .data(json.geometries)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .style("stroke", "#fff")
-        .style("stroke-width", "1")
-        .style("fill", function (d) {
-          // Get data value
-          var value = d.properties.name;
+    // Bind the data to the SVG and create one path per GeoJSON feature
+    svg
+      .selectAll("path")
+      .data(json.geometries)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("stroke", "#fff")
+      .style("stroke-width", "1")
+      .style("fill", function (d) {
+        // Get data value
+        var value = d.properties.name;
 
-          if (value) {
-            //If value exists…
-            return color(value);
-          } else {
-            //If value is undefined…
-            return "rgb(213,222,217)";
-          }
-        });
+        if (value) {
+          //If value exists…
+          return color(value);
+        } else {
+          //If value is undefined…
+          return "rgb(213,222,217)";
+        }
+      });
 
-      // Map the cities I have lived in!
+    // Map the cities I have lived in!
 
-      svg
-        .selectAll("circle")
-        .data(formattedData)
-        .enter()
-        .append("circle")
-        // .attr("cx", function (d) {
-        //   return projection([d.lon, d.lat])[0];
-        // })
-        // .attr("cy", function (d) {
-        //   return projection([d.lon, d.lat])[1];
-        // })
-        // .attr("r", function (d) {
-        //   return Math.sqrt(d.years) * 4;
-        // })
-        .style("fill", "rgb(217,91,67)")
-        .style("opacity", 0.85)
+    svg
+      .selectAll("circle")
+      .data(formattedData)
+      .enter()
+      .append("circle")
+      // .attr("cx", function (d) {
+      //   return projection([d.lon, d.lat])[0];
+      // })
+      // .attr("cy", function (d) {
+      //   return projection([d.lon, d.lat])[1];
+      // })
+      // .attr("r", function (d) {
+      //   return Math.sqrt(d.years) * 4;
+      // })
+      .style("fill", "rgb(217,91,67)")
+      .style("opacity", 0.85)
 
-        // Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks"
-        // http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
-        .on("mouseover", function (d) {
-          div.transition().duration(200).style("opacity", 0.9);
-          div
-            .text(d.dimension_values)
-            .style("left", d3.event.pageX + "px")
-            .style("top", d3.event.pageY - 28 + "px");
-        })
+      // Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks"
+      // http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
+      .on("mouseover", function (d) {
+        div.transition().duration(200).style("opacity", 0.9);
+        div
+          .text(d.dimension_values)
+          .style("left", d3.event.pageX + "px")
+          .style("top", d3.event.pageY - 28 + "px");
+      })
 
-        // fade out tooltip on mouse out
-        .on("mouseout", function (d) {
-          div.transition().duration(500).style("opacity", 0);
-        });
+      // fade out tooltip on mouse out
+      .on("mouseout", function (d) {
+        div.transition().duration(500).style("opacity", 0);
+      });
 
-      // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
-      var legend = d3
-        .select("body")
-        .append("svg")
-        .attr("class", "legend")
-        .attr("width", 140)
-        .attr("height", 200)
-        .selectAll("g")
-        .data(color.domain().slice().reverse())
-        .enter()
-        .append("g")
-        .attr("transform", function (d, i) {
-          return "translate(0," + i * 20 + ")";
-        });
+    // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
+    var legend = d3
+      .select("body")
+      .append("svg")
+      .attr("class", "legend")
+      .attr("width", 140)
+      .attr("height", 200)
+      .selectAll("g")
+      .data(color.domain().slice().reverse())
+      .enter()
+      .append("g")
+      .attr("transform", function (d, i) {
+        return "translate(0," + i * 20 + ")";
+      });
 
-      legend
-        .append("rect")
-        .attr("width", 18)
-        .attr("height", 18)
-        .style("fill", color);
+    legend
+      .append("rect")
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
 
-      legend
-        .append("text")
-        .data(legendText)
-        .attr("x", 24)
-        .attr("y", 9)
-        .attr("dy", ".35em")
-        .text(function (d) {
-          return d;
-        });
-    }
-  );
+    legend
+      .append("text")
+      .data(legendText)
+      .attr("x", 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .text(function (d) {
+        return d;
+      });
+  });
 
   //novo fim
   return svg;
