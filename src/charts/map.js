4393c4b29af601aee06eb5874f79bfd28713cf79
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-05-19 14:55:15 -0300

    Criação e manutenção d novos charts

--- src/charts/map.js
+++ src/charts/map.js
@@ -91,14 +91,6 @@ export function mapChart(params) {
     var div = d3.select("#toolTip");
   }
 
-  d3.select("#chart").attr("style", "overflow:hidden")
-    .html(`<h3 style="position:absolute; margin-left:10px;margin-top:8px;">
-                        <span style="font-family: ${fontFamily}; font-weight:${fontWeightNormal} ;
-                       ">     
-                        ${titleChart}
-                        </span>
-                        </h3>`);
-
   d3.select("#chart").attr("style", "overflow:hidden").html(`<h3 style="
     font-weight: bold;
     font-size: 88px;
