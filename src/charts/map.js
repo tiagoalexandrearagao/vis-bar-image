1fc43f79a6cfa18aaade0ff6824689e898ad0b2a
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-05-22 15:09:34 -0300

    Criação e manutenção d novos charts

--- src/charts/map.js
+++ src/charts/map.js
@@ -206,7 +206,7 @@ export async function mapChart(params) {
   //var range = ["#005954 ", "#9ce0db"];
   var rangeLegend = ["#005954 ", "#9ce0db "];
 
-  var range = ["#005954", "#f8de7e", "#dc143c"]; //verde - amarelo - vermelho
+  var range = ["#005954", "#f8de7e"]; //verde - amarelo - vermelho"#dc143c"
 
   //var colorScale = d3.scaleThreshold().domain(domain).range(range);
 
