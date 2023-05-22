27c8261a0edb023d0f52d15be2db2c250e6bd687
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-05-22 18:15:16 -0300

    Criação e manutenção d novos charts

--- src/charts/map.js
+++ src/charts/map.js
@@ -335,6 +335,7 @@ export async function mapChart(params) {
     //remove styling when the mouse leaves.
     .on("mouseout", function (d, i) {
       d3.select(this).style("stroke", "#fff");
+      div.style("display", "none");
     })
     .style("opacity", 0.7);
 
