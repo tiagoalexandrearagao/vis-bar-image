3a38c6840f7dfe85b994b02a7613c808b75b331d
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-05-22 11:13:54 -0300

    Criação e manutenção d novos charts

--- src/charts/map.js
+++ src/charts/map.js
@@ -231,7 +231,7 @@ export async function mapChart(params) {
       .reduce(
         (m, o) =>
           m.set(
-            o.dimension_values,
+            m.properties.name,
             Object.assign(m.get(o.dimension_values) || {}, o)
           ),
         new Map()
