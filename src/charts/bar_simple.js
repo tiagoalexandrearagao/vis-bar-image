c2e76ed1991e70d4419b6b3313aabd61555d6ee8
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-06-14 13:02:50 -0300

    Correções

--- src/charts/bar_simple.js
+++ src/charts/bar_simple.js
@@ -281,7 +281,7 @@ export function barSimpleChart(params) {
     .enter()
     .append("text")
     .text(function (d) {
-      if (hideMeasure == "yes") {
+      if (hideMeasure == "no") {
         countHiddenEvenColumns++;
         if (isHiddenEvenColumns == true) {
           if (countHiddenEvenColumns % 2 == 0) {
