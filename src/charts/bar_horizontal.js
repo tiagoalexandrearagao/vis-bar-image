399b348763e0907c93ad8895060bf591bcf8d282
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-06-15 19:40:01 -0300

    Correções

--- src/charts/bar_horizontal.js
+++ src/charts/bar_horizontal.js
@@ -269,7 +269,7 @@ export function barHorizontalChart(params) {
     .data(formattedData)
     .enter()
     .append("text")
-    .attr("x", width + 60)
+    .attr("x", width + 40)
     .attr("y", function (d, i) {
       return newY(i) + yRangeBand / 2;
     })
