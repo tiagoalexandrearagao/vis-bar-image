721c55d162eb0c92b0e18a7736cd15f5b4165320
tiagoalexandrearagao <tiago.alexandre@g.globo>
2023-11-09 17:07:37 -0300

    Commit final

---
+++ src/charts/bar/common/index.js
@@ -0,0 +1,13 @@
+export const options = {
+  title_graphic: {
+    section: "1. Main",
+    type: "string",
+    label: "1 Title",
+  },
+  default_icon: {
+    section: "1. Main",
+    type: "string",
+    label: "2. Title Icon <i>",
+    default: "",
+  },
+};
